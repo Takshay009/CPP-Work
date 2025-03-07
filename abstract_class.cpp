545cf99bebd28f5c58d4b1d13751fd0362ae5331
Takshay009 <takshaybhalodiya183@gmail.com>
2025-03-07 22:22:09 +0530

    give a space

--- abstract_class.cpp
+++ abstract_class.cpp
@@ -4,8 +4,10 @@ using namespace std;
 // if any pure virtual function is present in the class, then the class is known as an abstract class.
 class shape
 {
+
 public:
     virtual void draw() = 0; // pure virtual function
+    
 };
 
 class circle : public shape
